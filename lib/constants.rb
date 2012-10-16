bc26114c026f22926d90a8e7b55912cdbada2f37
De Wet Blomerus <dewet@blomerus.org>
2012-10-16 20:34:07 +0200

    Allows passing in namespaces constants to post_ad

---
+++ lib/constants.rb
@@ -0,0 +1,15 @@
+# A collection of Gumtree constants
+
+module Categories
+  module Home_Garden
+    FURNITURE = "9181"
+  end
+  module Electronics
+    MOBILE_PHONES = "9198"
+    COMPUTERS_SOFTWARE = "9199"
+  end
+  module Services
+    PHOTOGRAPHY_VIDEO = "9115"
+  end
+end
+
